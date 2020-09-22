10b4ac320602c0972ad39d159592d26719f6c05c
Erian Erik dos Santos Silva <erianerik@brq.com>
2020-09-22 15:57:45 -0300

    Implementando validação no formulário

--- js/cadastroLogin.js
+++ js/cadastroLogin.js
@@ -1,14 +1,23 @@
 const regexEmail = RegExp("^[a-zA-Z0-9$%!._-]+\@[a-z]+\.[a-z]{2,4}$", "g");
+const regexPassword = RegExp("^[0-9]{6,8}$", "g");
 
-$(".ancor-next").click(function() {
+$(".validation-container").hide();
+
+function validationField(inputEmail, regexEmail, inputPassword, regexPassword) {
+    return inputEmail.match(regexEmail) && inputPassword.match(regexPassword);
+}
+
+$(".ancor-next").click(function () {
+    $("#input-email").removeClass("error");
     let inputEmail = $("#input-email").val();
-    let inputPassword = $("input-password").val();
+    let inputPassword = $("#input-password").val();
 
-
-    if(inputEmail.match(regexEmail)) {
-        console.log("Foi");
-    }else {
-        console.log("Invalido");
+    if (validationField(inputEmail, regexEmail,inputPassword, regexPassword )) {
+        window.location.href = "./cadastroUsuario.html";
+    } else {
+        $(".validation-container").show();
+        $("#input-email").addClass("error");
+        $("#input-password").addClass("error");
     }
 
 })
